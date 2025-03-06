82b5860ab9e51679bb55f746a247c55801af2744
NiniKu <niniku1457@google.com>
2025-03-06 14:11:48 +0800

    -

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,45 @@
+//!ClientLayoutRootLayout!
+'use client';
+import Link from 'next/link';
 import '../styles/global.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+import { useSelectedLayoutSegment } from 'next/navigation';
+import FadeInComponent from '../components/fadeInComponent';
+//#font 
 import { spFont } from './utils/font';
-import ClientLayout from './layout';
 
-export const metadata = {
-    title: "My Portfolio",
-    icons: {
-        icon: "/react-projects/me.png",
-    },
-};
+const projects = ["Todolist", "Calculator", "About", "FileReader"];
 
 export default function RootLayout({ children }) {
+    const activeSegment = useSelectedLayoutSegment();
+    const currentPage = activeSegment
+        ? projects.find((el) => el.toLowerCase() === activeSegment) || "Unknown Page"
+        : "Home Page";
+
     return (
         <html lang="en">
             <body className={spFont}>
-                <ClientLayout>
-                    {children}
-                </ClientLayout>
+                <div id="root" >
+                    <div className={`App ${activeSegment || ""}`}>
+                        <header className="App-header">
+                            <h1 className='title'>{currentPage}</h1>
+                            {activeSegment && (
+                                <Link href='/' className="backHM">
+                                    <FontAwesomeIcon className="text-xl p-3" icon="fa-solid fa-house-chimney-window" />
+                                </Link>
+                            )}
+                        </header>
+                        <FadeInComponent>
+                            {children}
+                        </FadeInComponent>
+                    </div>
+                </div>
             </body>
         </html>
+
     );
 }
